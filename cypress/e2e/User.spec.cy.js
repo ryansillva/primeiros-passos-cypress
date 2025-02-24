cb514449cc9fe51e831430bd0056c950e5b2f6ea
Ryan <ryansamuel432@gmail.com>
2025-02-24 17:43:43 -0300

    added login page, added login spec and login scenario update

--- cypress/e2e/User.spec.cy.js
+++ cypress/e2e/User.spec.cy.js
@@ -22,21 +22,5 @@ describe('Orange HRM Tests', () => {
     myInfoPage.fillEmployeeDetails('EmployID', 'OtherID', 'Drivers Number', '2025-03-10')
     myInfoPage.fillStatus()
     myInfoPage.saveForms()
-    
-    /*cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
-    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
-    cy.get(selectorsList.genericField).eq(3).clear().type('Employee')
-    cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
-    cy.get(selectorsList.genericField).eq(5).clear().type('DriverLicenseTest')
-    cy.get(selectorsList.dateField).eq(0).clear().type('2025-03-10')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.submitButton).eq(0).click({force: true})
-    cy.get('body').should('contain', 'Successfully Update')
-    cy.get('.oxd-toast-close')
-    cy.get(selectorsList.genericComboBox).eq(0).click({force: true})
-    cy.get(selectorsList.secondItemComboBox).click()
-    cy.get(selectorsList.genericComboBox).eq(1).click({force: true})
-    cy.get(selectorsList.thirdItemComboBox).click()*/
-
   })
 })
